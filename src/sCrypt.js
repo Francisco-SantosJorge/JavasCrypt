51f37c9fee02110770e902abf9a33759aaf8a403
Kiko <francisco.santos-jorge@etu.univ-amu.fr>
2026-01-22 12:06:00 +0100

    Partie 4 et 5.

--- src/sCrypt.js
+++ src/sCrypt.js
@@ -12,5 +12,10 @@ let I_cpt = -1;
 const I_idInterval = setInterval(() => {
     I_cpt++;
     O_resultat.innerText = A_randomTable[I_cpt].toString();
+    O_resultat.classList.remove("blue", "green", "orange", "red");
+    if (A_randomTable[I_cpt] < 0) O_resultat.classList.add("blue");
+    else if (A_randomTable[I_cpt] < 20) O_resultat.classList.add("green");
+    else if (A_randomTable[I_cpt] < 30) O_resultat.classList.add("orange");
+    else O_resultat.classList.add("red");
     if (I_cpt >= A_randomTable.length - 1) clearInterval(I_idInterval);
 }, 2000);
