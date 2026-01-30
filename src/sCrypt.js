a8cb8916846b6d3940a9dd6867a058f7d5794cbf
Kiko <francisco.santos-jorge@etu.univ-amu.fr>
2026-01-30 16:19:44 +0100

    Fin du TD1 + 1 du TD2

--- src/sCrypt.js
+++ src/sCrypt.js
@@ -10,12 +10,31 @@ const O_resultat = document.getElementById("output");
 
 let I_cpt = -1;
 const I_idInterval = setInterval(() => {
+    let O_toDelete = document.getElementById("alert");
+    if (O_toDelete != null) {
+        O_toDelete.remove();
+    }
     I_cpt++;
+    const O_alert = document.createElement('p')
+    O_alert.id = "alert";
+    O_alert.role = "alert";
     O_resultat.innerText = A_randomTable[I_cpt].toString();
     O_resultat.classList.remove("blue", "green", "orange", "red");
-    if (A_randomTable[I_cpt] < 0) O_resultat.classList.add("blue");
-    else if (A_randomTable[I_cpt] < 20) O_resultat.classList.add("green");
-    else if (A_randomTable[I_cpt] < 30) O_resultat.classList.add("orange");
-    else O_resultat.classList.add("red");
+    if (A_randomTable[I_cpt] < 0) {
+        O_resultat.classList.add("blue");
+        O_alert.innerText = "Brrrrrrr, un peu froid ce matin, mets ta cagoule !";
+        O_resultat.before(O_alert);
+    }
+    else if (A_randomTable[I_cpt] < 20) {
+        O_resultat.classList.add("green");
+    }
+    else if (A_randomTable[I_cpt] < 30) {
+        O_resultat.classList.add("orange");
+    }
+    else {
+        O_resultat.classList.add("red");
+        O_alert.innerText = "Caliente ! Vamos a la playa, ho hoho hoho !!";
+        O_resultat.before(O_alert);
+    }
     if (I_cpt >= A_randomTable.length - 1) clearInterval(I_idInterval);
-}, 2000);
+}, 4000);
